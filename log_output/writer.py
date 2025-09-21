0956a62a837ef9fc61fa4b3a732b50505b7cbefb
JVilo <vilo.johanna@gmail.com>
2025-09-21 12:03:59 +0300

    task 1.10 done

---
+++ log_output/writer.py
@@ -0,0 +1,12 @@
+import time
+import uuid
+from datetime import datetime, timezone
+
+random_string = str(uuid.uuid4())
+file_path = "/shared/log.txt"  # path inside the shared volume
+
+while True:
+    now = datetime.now(timezone.utc).isoformat(timespec='milliseconds')
+    with open(file_path, "a") as f:
+        f.write(f"{now}: {random_string}\n")
+    time.sleep(5)
